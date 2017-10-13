a35000ed5d8446cfe97ae8bb818b42261e01265b
Stephen Klein <stephendklein08@gmail.com>
2017-10-13 05:19:57 +0000

    Added notice and styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  
+#Run the code below before any other code is run in this controller.
+#Only occure when the user goes to the sign up pages.
+  before_filter :select_plan, only: :new
+  
+  
   def create
     super do |resource|
       if params[:plan]
@@ -9,6 +15,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
           resource.save
         end
       end
+    end
+  end
+      
+      #Function- unless the user comes from a button with 1 or 2 for plan parameter send user back to home page.
+                       
+  private
+  def select_plan
+    unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+      flash[:notice] = "Please select a membership plan to sign up."
+      redirect_to root_url
     end
   end
 end
